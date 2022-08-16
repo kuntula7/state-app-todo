c92bf43b5c45034173e4ff0bb0bd3021fc4b170d
beqa kereselidze <beqa.kereselidze@gmail.com>
2022-08-16 12:22:12 +0400

    first commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () =>{
+    return(
+        <header className="hd">
+            <h1>
+                My ToDos
+            </h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/ToDoList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ToDo from "./ToDo";
+
+const ToDoList = ({toDoList,handleToggle,handleFilter})=>{
+    return(
+        <div className="list">
+            {toDoList.map(todo=>{
+                return(
+                    <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter}/>
+                )
+            })}
+            <button style={{margin: '20px'}} onClick={handleFilter}>Clear All</button>
+        </div>
+    )
+}
+export default ToDoList
